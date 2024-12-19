1159a0089cc40aef37b7e095529c9354cf189b06
cphkev <cph-ks427@cphbusiness.dk>
2024-12-19 10:22:07 +0100

    backend med security og nogle admin funktioner

--- src/main/java/dat/security/daos/ISecurityDAO.java
+++ src/main/java/dat/security/daos/ISecurityDAO.java
@@ -4,8 +4,14 @@ import dat.security.entities.User;
 import dat.security.exceptions.ValidationException;
 import dk.bugelhartmann.UserDTO;
 
+import java.util.List;
+
 public interface ISecurityDAO {
     UserDTO getVerifiedUser(String username, String password) throws ValidationException;
     User createUser(String username, String password);
     User addRole(UserDTO user, String newRole);
+
+    List<UserDTO> getAllUsers();
+
+    User updateUserRole(String username, String newRole);
 }
